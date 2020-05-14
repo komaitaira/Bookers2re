0d225a776a22b3885519963891c3fbb792967a0c
vagrant <vagrant@localhost.localdomain>
2020-05-14 13:25:10 +0000

    edit,update,destroy作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,9 @@ class BooksController < ApplicationController
   end
 
   def show
-    # @user = current_user
+    @user = current_user
     @book = Book.find(params[:id])
+
   end
 
   def create
@@ -19,12 +20,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
