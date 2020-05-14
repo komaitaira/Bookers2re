61efb42f33a4e9203512118d4cc9052ad7efcef7
vagrant <vagrant@localhost.localdomain>
2020-05-14 07:01:32 +0000

    homeのview-top/about作成,ログイン画面にnameのフォームを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
-# sign upの際にnameカラムを利用
+# sign_up/inの際にnameカラムを利用
 end
