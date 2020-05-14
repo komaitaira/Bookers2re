88eb7d35ce96207396bb6b05e3ed60d5a31c0319
vagrant <vagrant@localhost.localdomain>
2020-05-14 11:53:29 +0000

    uses/show.html.erb作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,21 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+
 
   def index
   end
 
   def show
+    # @user = current_user
+    @book = Book.find(params[:id])
   end
 
   def create
+    @user = current_user
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def edit
@@ -16,6 +25,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  # (ログインしていない状態で他のページに遷移しようとした場合、ログインページに遷移する)
 
   def index
   end
 
   def show
+  	@book = Book.new
+  	@user = User.find(params[:id])
+  	@books = @user.books
   end
 
   def edit
