a63bf9575aa971bbc202244181c25eed3a2f16a3
vagrant <vagrant@localhost.localdomain>
2020-05-14 08:17:13 +0000

    bookモデルを作成しuserモデルに関連付け

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  resources :users
   root 'home#top'
   get 'home/about'
   # rootがわからない, ログイン後のリダイレクト先　users/show
-  resources :books
-  resources :users
 end
