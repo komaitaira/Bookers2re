5c3b53e9c5523f897592b21686cbc99cce2be38b
vagrant <vagrant@localhost.localdomain>
2020-05-18 05:30:02 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -60,7 +60,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     # belongs_toのおかげでbookオブジェクトからuserオブジェクトへアクセスできる。
     if book.user.id != current_user.id
-      redirect_to user_path(current_user.id)
+      redirect_to books_path
     end
   end
 
